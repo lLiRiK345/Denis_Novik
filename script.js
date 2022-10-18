0e0d7cb8d2232d694de324c487b1ea3d1819c4b0
lLiRiK345 <emelkir345@gmail.com>
2022-10-18 19:44:14 +0300

    Error

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-const popUp = document.querySelector(".popup");
-const close = document.querySelector(".close");
+const popUp = document.querySelector('.popup')
+const burger = document.querySelector('.burger')
+const burger_top = document.querySelector('.burger_top')
+const burger_bot = document.querySelector('.burger_bot')
+const close = document.querySelectorAll('.close')
 
-close.addEventListener("click", () => {
-  popUp.classList.toggle("popUp-active");
-  if (popUp.classList.contains("popUp-active")) {
-    document.body.style.overflow = "hidden";
+burger.addEventListener('click', () => {
+  console.log(close);
+  close[0].classList.toggle('close_right')
+  close[1].classList.toggle('close_left')
+  popUp.classList.toggle('popUp-active')
+  burger_top.classList.toggle('hidden')
+  burger_bot.classList.toggle('hidden')
+  console.dir(close);
+  if (popUp.classList.contains('popUp-active')) {
+    document.body.style.overflow = 'hidden'
   } else {
-    document.body.style.overflow = "";
+    document.body.style.overflow = ''
   }
-});
+})
